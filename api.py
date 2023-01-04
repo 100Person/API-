1217be0497dd649742cebe0e346b3e4d4b0141a4
100Person <121074713+100Person@users.noreply.github.com>
2023-01-04 17:10:59 +0700

    Create api.py

---
+++ api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    text: str
+
+app = FastAPI()
+translator  = pipeline ("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
+
+@app.get("/")
+def root():
+    return {"message": " Я люблю узнавать новое!"}
+
+@app.post("/predict/")
+def predict(item: Item):
+    return translator(item.text )[0]
